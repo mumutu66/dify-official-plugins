e171a2a722e29e01607318fa85989ed80f32094f
mumutu66 <30490822+mumutu66@users.noreply.github.com>
2025-02-20 16:41:54 +0800

    Update llm.py
    test \n

--- extensions/oaicompat_dify_model/endpoints/llm.py
+++ extensions/oaicompat_dify_model/endpoints/llm.py
@@ -132,7 +132,7 @@ class OaicompatDifyModelEndpoint(Endpoint, BaseAuth):
                                 ],
                             }
                         )
-                        + "\n\n"
+                        + "\n"
                     )
 
         return Response(
